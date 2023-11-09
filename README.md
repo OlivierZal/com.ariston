245642432c18c60696484c1aca7cdaa1673dd2a4
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2023-11-09 23:00:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Ariston for Homey
+
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=OlivierZal_com.ariston&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=OlivierZal_com.ariston)
+
+## Introduction
+
+## Usage
+
+1. You must have a Homey Pro.
+2. Install the [Ariston Homey app](https://homey.app/a/com.ariston) from the Homey App Store.
+3. Pair your devices.
---
+++ eslint.config.js
@@ -0,0 +1,109 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const tsParser = require('@typescript-eslint/parser')
+const tsPlugin = require('@typescript-eslint/eslint-plugin')
+const {
+  extends: airbnbRules,
+  ...airbnbConfig
+} = require('eslint-config-airbnb-base')
+const prettier = require('eslint-config-prettier')
+const importPlugin = require('eslint-plugin-import')
+const envs = require('globals')
+
+const envMapping = {
+  es6: 'es2015',
+  node: 'node',
+}
+
+function convertIntoEslintFlatConfig(config) {
+  const { env, globals, plugins, parserOptions, ...oldConfig } = config
+  return {
+    ...oldConfig,
+    languageOptions: {
+      ...('env' in config && {
+        globals: Object.fromEntries(
+          Object.keys(env)
+            .filter(
+              (key) => env[key] && key in envMapping && envMapping[key] in envs,
+            )
+            .flatMap((key) => Object.entries(envs[envMapping[key]])),
+        ),
+        ...('parserOptions' in config && {
+          parserOptions,
+        }),
+      }),
+    },
+  }
+}
+
+const customRules = {
+  'no-bitwise': 'off',
+  'no-underscore-dangle': [
+    'error',
+    {
+      allow: ['__'],
+    },
+  ],
+}
+const tsCustomRules = {
+  '@typescript-eslint/naming-convention': 'off',
+  '@typescript-eslint/no-magic-numbers': 'off',
+  '@typescript-eslint/no-unused-vars': [
+    'error',
+    {
+      varsIgnorePattern: 'onHomeyReady',
+    },
+  ],
+  '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+  'import/extensions': 'off',
+  'import/no-duplicates': ['error', { 'prefer-inline': true }],
+}
+
+module.exports = [
+  {
+    ignores: ['.homeybuild/'],
+  },
+  // eslint-disable-next-line global-require, import/no-dynamic-require
+  ...airbnbRules.map((rule) => convertIntoEslintFlatConfig(require(rule))),
+  convertIntoEslintFlatConfig(airbnbConfig),
+  {
+    rules: customRules,
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs['eslint-recommended'].overrides[0].rules,
+      ...tsPlugin.configs.all.rules,
+      ...tsCustomRules,
+    },
+  },
+  {
+    plugins: {
+      import: importPlugin,
+    },
+  },
+  importPlugin.configs.typescript,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      sourceType: 'module',
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+    },
+  },
+  prettier,
+]
