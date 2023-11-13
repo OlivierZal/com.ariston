6bf6890a3370a5a62c23dc93c6ed801b44cc1c9c
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2023-11-13 17:51:45 +0100

    improve error handling

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import { App } from 'homey' // eslint-disable-line import/no-extraneous-dependencies
 import axios from 'axios'
-import withAPI, { getAPIErrorMessage } from './mixins/withAPI'
+import withAPI, { getErrorMessage } from './mixins/withAPI'
 import type {
   LoginCredentials,
   LoginData,
@@ -10,16 +10,6 @@ import type {
 } from './types'
 
 axios.defaults.baseURL = 'https://www.ariston-net.remotethermo.com/api/v2'
-
-function getErrorMessage(error: unknown): string {
-  let errorMessage = String(error)
-  if (axios.isAxiosError(error)) {
-    errorMessage = getAPIErrorMessage(error)
-  } else if (error instanceof Error) {
-    errorMessage = error.message
-  }
-  return errorMessage
-}
 
 export = class AristonApp extends withAPI(App) {
   #loginTimeout!: NodeJS.Timeout
