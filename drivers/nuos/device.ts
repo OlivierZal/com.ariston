5e83a178855b72b45d7bea1df3de3a55631ac0ec
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2023-12-10 01:24:48 +0100

    prettier

--- drivers/nuos/device.ts
+++ drivers/nuos/device.ts
@@ -412,10 +412,7 @@ class NuosDevice extends withAPI(Device) {
     if (min === options.min && max === options.max) {
       return
     }
-    await this.setCapabilityOptions('target_temperature', {
-      min,
-      max,
-    })
+    await this.setCapabilityOptions('target_temperature', { min, max })
     await this.setWarning(this.homey.__('warnings.settings'))
   }
 }
