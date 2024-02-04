5a79dd19b1e0aace40f7f4b84528377eebaa246f
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-02-05 00:29:21 +0100

    improve

--- mixins/withAPI.ts
+++ mixins/withAPI.ts
@@ -78,6 +78,8 @@ const withAPI = <T extends HomeyClass>(base: T): APIClass & T =>
   class WithAPI extends base {
     public readonly api: AxiosInstance = axios.create()
 
+    public readonly app: AristonApp = this.homey.app as AristonApp
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     public constructor(...args: any[]) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
@@ -100,7 +102,7 @@ const withAPI = <T extends HomeyClass>(base: T): APIClass & T =>
       postData: PostData | null = null,
     ): Promise<{ data: GetData }> {
       return this.api<GetData>({
-        method: postData ? 'post' : 'get',
+        method: postData ? 'POST' : 'GET',
         url: `/R2/PlantHomeSlp/${postData ? 'SetData' : 'GetData'}/${id}`,
         ...(postData ? { data: postData } : {}),
         ...(postData
@@ -149,15 +151,14 @@ const withAPI = <T extends HomeyClass>(base: T): APIClass & T =>
       response: AxiosResponse,
     ): Promise<AxiosResponse> {
       this.log(getAPICallData(response).join('\n'))
-      const contentType = String(response.headers['content-type'])
-      const app: AristonApp = this.homey.app as AristonApp
       if (
-        contentType.includes('text/html') &&
-        app.retry &&
+        // @ts-expect-error: `axios` is partially typed
+        response.headers.hasContentType('application/json') === false &&
+        this.app.retry &&
         response.config.url !== LOGIN_URL
       ) {
-        app.handleRetry()
-        const loggedIn: boolean = await app.login()
+        this.app.handleRetry()
+        const loggedIn: boolean = await this.app.login()
         if (loggedIn) {
           return this.api.request(response.config)
         }
@@ -168,6 +169,17 @@ const withAPI = <T extends HomeyClass>(base: T): APIClass & T =>
     private async handleError(error: AxiosError): Promise<AxiosError> {
       const apiCallData: string[] = getAPICallData(error)
       this.error(apiCallData.join('\n'))
+      if (
+        error.response?.status === axios.HttpStatusCode.Unauthorized &&
+        this.app.retry &&
+        error.config?.url !== LOGIN_URL
+      ) {
+        this.app.handleRetry()
+        const loggedIn: boolean = await this.app.login()
+        if (loggedIn && error.config) {
+          return this.api.request(error.config)
+        }
+      }
       await this.setErrorWarning(apiCallData[apiCallData.length - 1])
       return Promise.reject(error)
     }
