b0e53588a6ba02144303b0e437027e63967d787f
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-02-26 18:18:55 +0100

    better

--- ariston/api.ts
+++ ariston/api.ts
@@ -51,11 +51,9 @@ export default class AristonAPI {
 
   readonly #api: AxiosInstance
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly #errorLogger: (...args: any[]) => void
+  readonly #errorLogger: Logger
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly #logger: (...args: any[]) => void
+  readonly #logger: Logger
 
   readonly #settingManager: SettingManager
 
